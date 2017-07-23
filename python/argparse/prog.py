78bd781e3289db163785bc510aa6937475746672
邱从贤 <qiucongxian@meituan.com>
2017-07-23 22:37:41 +0800

    add some code in prog.py

--- python/argparse/prog.py
+++ python/argparse/prog.py
@@ -4,10 +4,13 @@ import argparse
 parser = argparse.ArgumentParser()
 
 #square 是必填参数
+#type = int 表示输入的为 int
 parser.add_argument("square", help="display a square of a given number", type=int)
 #verbose 是可选参数, -v 是短选项
 #action = store_true 表示该参数可以不带值
-parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+#action = count 会记录该参数出现多少次
+#choice = [0, 1, 2] 表示输入只接受 0，1，2
+parser.add_argument("-v", "--verbose", type=int, choices=[0, 1, 2], help="increase output verbosity")
 
 args = parser.parse_args()
 
